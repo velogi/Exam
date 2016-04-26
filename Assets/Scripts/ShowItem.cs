ce5e3482d702da99396e3b9806ab3f01a98e9d11
velogi <newbieh.st@gmail.com>
2016-04-26 15:30:31 +0700

    Exam

---
+++ Assets/Scripts/ShowItem.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using Mono.Data.SqliteClient;
+using System.Data;
+using System;
+using UnityEngine.UI;
+public class ShowItem : MonoBehaviour
+{
+    public Text PlayerDataTxt;
+
+    void Start()
+    {
+        SelectItem();
+    }
+    public void SelectItem()
+    {
+        string connectionString = "URI=file:" + Application.dataPath + "/myDatabase.db"; //Path to database.
+        IDbConnection dbconn;
+        dbconn = (IDbConnection)new SqliteConnection(connectionString);
+        dbconn.Open(); //Open connection to the database.
+        IDbCommand dbcmd = dbconn.CreateCommand();
+        string sqlQuery = "select * from Account";
+        dbcmd.CommandText = sqlQuery;
+        IDataReader reader = dbcmd.ExecuteReader();
+        PlayerDataTxt.text = "";
+        while (reader.Read())
+        {
+            string userName = reader.GetString(1);
+            string passWord = reader.GetString(2);
+            int age = reader.GetInt32(3);
+            PlayerDataTxt.text += userName + "-" + passWord + "-" + age + "\n";
+        }
+        //reader.Close(); reader = null;
+        dbcmd.Dispose(); dbcmd = null;
+        dbconn.Close(); dbconn = null;
+    }
+}
